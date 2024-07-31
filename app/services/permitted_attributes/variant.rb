94294e08f084f73f190deceb1dab5361d7fed99f
Gaetan Craig-Riou <gaetan.riou@gmail.com>
2024-07-31 10:55:54 +1000

    S ? Fix rebase typo

--- app/services/permitted_attributes/variant.rb
+++ app/services/permitted_attributes/variant.rb
@@ -7,7 +7,7 @@ module PermittedAttributes
         :id, :sku, :on_hand, :on_demand, :shipping_category_id, :price, :unit_value,
         :unit_description, :variant_unit, :variant_unit_name, :variant_unit_scale, :display_name,
         :display_as, :tax_category_id, :weight, :height, :width, :depth, :taxon_ids,
-        :primary_taxon_id, supplier_id
+        :primary_taxon_id, :supplier_id
       ]
     end
   end
