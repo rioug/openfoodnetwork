80a12db1913c8ce568a6d6464bec75bcfd4e232b
Maikel Linke <maikel@email.org.au>
2026-03-31 11:20:23 +1100

    Move database clean from cron to Sidekiq scheduler
    After moving the remaining tasks from schedule.rb to sidekiq.yml, we can
    remove whenever and won't rely on cron any more. That will simplify the
    setup and migration to a new server.

---
+++ app/jobs/rake_job.rb
@@ -0,0 +1,13 @@
+# frozen_string_literal: true
+
+# Executes a rake task
+class RakeJob < ApplicationJob
+  def perform(task_string)
+    Rails.application.load_tasks if Rake::Task.tasks.empty?
+
+    Rake.application.invoke_task(task_string)
+  ensure
+    name, _args = Rake.application.parse_task_string(task_string)
+    Rake::Task[name].reenable
+  end
+end
--- config/schedule.rb
+++ config/schedule.rb
@@ -11,10 +11,6 @@ env "MAILTO", ENV["SCHEDULE_NOTIFICATIONS"] if ENV["SCHEDULE_NOTIFICATIONS"]
 # If we use -e with a file containing specs, rspec interprets it and filters out our examples
 job_type :run_file, "cd :path; :environment_variable=:environment bundle exec script/rails runner :task :output"
 
-every 1.month, at: '4:30am' do
-  rake 'ofn:data:remove_transient_data'
-end
-
 every 1.day, at: '2:45am' do
   rake 'db2fog:clean' if ENV['S3_BACKUPS_BUCKET']
 end
