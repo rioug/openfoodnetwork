c8192173df62da727512bad000faa29a9f2ee351
Gaetan Craig-Riou <gaetan.riou@gmail.com>
2026-04-17 13:52:01 +1000

    Calculate paid_with_credit when error on the summary step

--- app/controllers/checkout_controller.rb
+++ app/controllers/checkout_controller.rb
@@ -64,7 +64,8 @@ class CheckoutController < BaseController
   private
 
   def render_error
-    @paid_with_credit = calculate_credit(@order) if payment_step?
+    # TODO fix this , error on moving to summary ?
+    @paid_with_credit = calculate_credit(@order) if payment_step? || summary_step?
 
     flash.now[:error] ||= I18n.t('checkout.errors.saving_failed')
 
