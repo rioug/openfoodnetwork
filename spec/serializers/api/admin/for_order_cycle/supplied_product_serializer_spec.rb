c90acf0b47c014ee7dbc567de4edc080cc65dbd2
Gaetan Craig-Riou <gaetan.riou@gmail.com>
2026-04-22 11:43:37 +1000

    Apply product_selection_from_coordinator_inventory_only when inventory enabled
    Pass inventory_enabled as an option. I don't thing the filtering should
    be happening in the serializer, but we are getting rid of inventory so
    there is no point fixing it now.

--- spec/serializers/api/admin/for_order_cycle/supplied_product_serializer_spec.rb
+++ spec/serializers/api/admin/for_order_cycle/supplied_product_serializer_spec.rb
@@ -1,10 +1,13 @@
 # frozen_string_literal: true
 
 RSpec.describe Api::Admin::ForOrderCycle::SuppliedProductSerializer do
-  subject(:serialized_product) { described_class.new(product, order_cycle:).to_json }
+  subject(:serialized_product) {
+    described_class.new(product, order_cycle:, inventory_enabled:).to_json
+  }
 
   let(:coordinator) { create(:distributor_enterprise) }
   let(:order_cycle) { instance_double(OrderCycle, coordinator:) }
+  let(:inventory_enabled) { false }
   let!(:product) { create(:simple_product) }
   let!(:non_inventory_variant) { product.variants.first }
   let!(:inventory_variant) { create(:variant, product: product.reload) }
@@ -12,33 +15,41 @@ RSpec.describe Api::Admin::ForOrderCycle::SuppliedProductSerializer do
     create(:inventory_item, enterprise: coordinator, variant: inventory_variant, visible: true)
   }
 
-  context "when order cycle shows only variants in the coordinator's inventory" do
-    before do
-      allow(order_cycle).to receive(:prefers_product_selection_from_coordinator_inventory_only?) {
-                              true
-                            }
-    end
+  describe "variants" do
+    context "when order cycle shows only variants in the coordinator's inventory" do
+      before do
+        allow(order_cycle).to receive(:prefers_product_selection_from_coordinator_inventory_only?) {
+                                true
+                              }
+      end
 
-    describe "variants" do
-      it "renders only variants that are in the coordinators inventory" do
-        expect(serialized_product).to have_json_size(1).at_path 'variants'
-        expect(serialized_product).to be_json_eql(inventory_variant.id).at_path 'variants/0/id'
+      it "ignores the setting and renders all variants" do
+        expect(serialized_product).to have_json_size(2).at_path 'variants'
+      end
+
+      context "when inventory enabled" do
+        let(:inventory_enabled) { true }
+
+        it "renders only variants that are in the coordinators inventory" do
+          expect(serialized_product).to have_json_size(1).at_path 'variants'
+          expect(serialized_product).to be_json_eql(inventory_variant.id).at_path 'variants/0/id'
+        end
       end
     end
-  end
 
-  context "when order cycle shows all available products" do
-    before do
-      allow(order_cycle).to receive(:prefers_product_selection_from_coordinator_inventory_only?) {
-                              false
-                            }
-    end
+    context "when order cycle shows all available products" do
+      before do
+        allow(order_cycle).to receive(:prefers_product_selection_from_coordinator_inventory_only?) {
+                                false
+                              }
+      end
 
-    describe "supplied products" do
-      it "renders variants regardless of whether they are in the coordinators inventory" do
-        expect(serialized_product).to have_json_size(2).at_path 'variants'
-        variant_ids = parse_json(serialized_product)['variants'].pluck('id')
-        expect(variant_ids).to include non_inventory_variant.id, inventory_variant.id
+      describe "supplied products" do
+        it "renders variants regardless of whether they are in the coordinators inventory" do
+          expect(serialized_product).to have_json_size(2).at_path 'variants'
+          variant_ids = parse_json(serialized_product)['variants'].pluck('id')
+          expect(variant_ids).to include non_inventory_variant.id, inventory_variant.id
+        end
       end
     end
   end
