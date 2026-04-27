6201307e2957bfeb187f2b78b28cf9b99791d43f
Gaetan Craig-Riou <gaetan.riou@gmail.com>
2026-04-27 14:35:33 +1000

    Remove model aliases decprecation
    It was fixed in https://github.com/openfoodfoundation/openfoodnetwork/pull/13951

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -74,9 +74,6 @@ Rails.application.configure do
     allowed_warnings = [
       # List strings here to allow matching deprecations.
       #
-      # Spree::CreditCard model aliases `cc_type` and has a method called `cc_type=` defined. Starting in Rails 7.2 `brand=` will not be calling `cc_type=` anymore. You may want to additionally define `brand=` to preserve the current behavior.
-      "model aliases",
-
       # Setting action_dispatch.show_exceptions to true is deprecated. Set to :all instead.
       # spec/requests/errors_spec.rb
       "action_dispatch.show_exceptions",
